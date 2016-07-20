442d1d563c88bf727fbf10e41b3306486d130df6
Logan Wright <logan.william.wright@gmail.com>
2016-07-19 23:27:08 -0400

    remove unnecessary dependencies

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Polymorphic",
-    dependencies: [
-        //Standards package. Contains protocols for cross-project compatability.
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 9)
-    ]
+    name: "Polymorphic"
 )
